8ffd2736625031aaa6dc96628f63744451f72714
Ali Güçlü <aliguclutr@gmail.com>
2020-12-18 20:54:28 +0300

    Update CallBack.php

--- CallBack.php
+++ CallBack.php
@@ -6,11 +6,9 @@ $payhesap = new Payhesap();
 
 $callback = $payhesap->callback();
 
-if ($callback != null) {
-	echo "Ödeme Başarılı.<br>";
-} else {
+if ($callback == null) {
 	echo "Ödeme Başarısız.<br>";
 	echo $payhesap->get_error();
+} else {
+	echo "Ödeme Başarılı.<br>";
 }
-?>
-<pre><?php print_r($_REQUEST); ?></pre>
